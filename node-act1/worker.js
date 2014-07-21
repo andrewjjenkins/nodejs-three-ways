1f1ada7be9490475d6278eb475d22eb1bc6fcadd
Andrew Jenkins <andrewjjenkins@gmail.com>
2014-07-21 15:06:09 -0600

    Catch pubs to notifications channel in redis and dump them

--- node-act1/worker.js
+++ node-act1/worker.js
@@ -1,1 +1,8 @@
-console.log("Hello OSCON");
+var redis = require('redis');
+
+var pubSubClient = redis.createClient('6379', 'localhost');
+
+pubSubClient.subscribe('notifications');
+pubSubClient.on('message', function(channel, message) {
+  console.log('Received a message: ', message);
+});
