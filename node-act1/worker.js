7ef44c8d8d10eaea94ef99649a6aaae4ba2c8e65
Andrew Jenkins <andrewjjenkins@gmail.com>
2014-07-21 15:16:13 -0600

    Refactor into config, pull down nodemailer

--- node-act1/worker.js
+++ node-act1/worker.js
@@ -1,6 +1,8 @@
-var redis = require('redis');
+var redis = require('redis')
+  , config = require('./config')
+;
 
-var pubSubClient = redis.createClient('6379', 'localhost');
+var pubSubClient = redis.createClient(config.port, config.host);
 
 pubSubClient.subscribe('notifications');
 pubSubClient.on('message', function(channel, message) {
